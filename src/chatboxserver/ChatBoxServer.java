8d70db40dc1fe6ac48a76132f86784d83fc3163d
Michael <dr.mike.attia@gmail.com>
2017-02-02 08:26:36 +0200

    add fx project

---
+++ src/chatboxserver/ChatBoxServer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatboxserver;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author michael
+ */
+public class ChatBoxServer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ChatBoxServerFXMLDoc.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setMinWidth(700);
+        stage.setMinHeight(650);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
